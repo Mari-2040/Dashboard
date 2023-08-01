a7f8707ffbfd24629724454ef1c8dae816bf8c9e
mariami <mdoluashvili@gmail.com>
2023-08-01 10:44:06 +0400

    afg

--- src/Components/AtherPage/AtherPage.tsx
+++ src/Components/AtherPage/AtherPage.tsx
@@ -1,11 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AtherPage() {
+  const emojis = [
+    "😀",
+    "😂",
+    "😍",
+    "🥹",
+    "🥰",
+    "😛",
+    "🤪",
+    "🤨",
+    "😏",
+    "🥺",
+    "😤",
+    "😡",
+    "🤬",
+    "🥶",
+    "😱",
+    "🤔",
+    "🫢",
+    "😐",
+    "😮",
+    "😵",
+    "🤨",
+    "😴",
+    "🙄",
+    "🤑",
+    "👽",
+    "❤️",
+    "💞",
+    "♋️",
+  ];
+
+  const [currentEmoji, setCurrentEmoji] = useState("");
+  const getRandomEmoji = () => {
+    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+    setCurrentEmoji(randomEmoji);
+  };
+
   return (
-    <div>
-      <div>
-        <div className='specific-prop-params-demo'>
-          <div className='eli'></div>
+    <div className="flex">
+      <section>
+        <div className='row'>
+          <div className='column'>
+            <div className='card'>
+              <div className='cover item-a'>
+                <h1>west</h1>
+                <button className='onck'> onclick </button>
+              </div>
+            </div>
+          </div>
+          <div className='column'>
+            <div className='card'>
+              <div className='cover item-b'>
+                <h1>East</h1>
+                <button className='onck2'> onclick</button>
+              </div>
+            </div>
+          </div>
+          <div className='column'>
+            <div className='card'>
+              <div className='cover item-c'>
+                <h1>Gold</h1>
+                <button className='onck3'> onclick</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="mt-[4rem] ml-[70rem]">
+        <div className='conainer'>
+          <h2>Random Emoji</h2>
+          <p id='emoji-container'>{currentEmoji}</p>
+          <button onClick={getRandomEmoji}>Generate</button>
+          <div className='circle1'></div>
+          <div className='circle2'></div>
         </div>
       </div>
     </div>
---
+++ src/Components/AtherPage/Atherconsole.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Atherconsole = () => {
+  return <div>
+     {/* <div className='specific-prop-params-demo'>
+        <div className='eli'></div>
+      </div> */}
+      {/* <div className=" ml-6 flex gap-3">
+          <input className="input" pattern="/d+" placeholder="mari"></input>
+          <input className="input" pattern="/d+" placeholder="age"></input>
+          <input className="input" pattern="/d+" placeholder="name"></input>
+          <input className="input" pattern="/d+" placeholder="email"></input>
+        </div>
+      <div className='light mt-10'>
+        <div className='beam'></div>
+        <div className='hilt'>
+          <div className='crown'></div>
+          <div className='stripes'>
+            <div className='part'></div>
+            <div className='part'></div>
+            <div className='part'></div>
+          </div>
+          <div className='part-circle'></div>
+        </div>
+      </div> */}
+  </div>;
+};
+
+export default Atherconsole;
